b692c3b9895e8599afd6106ff70cfc7438231f8e
ang92 <ang_92@list.ru>
2023-04-14 17:14:18 +0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,10 @@
+// Структура сервера
+
+import express from "express"
+
+const app = express()
+const PORT = 5000
+
+app.listen(PORT, () => {
+    console.log("Server has been started on port 5000...");
+})
